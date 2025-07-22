1d3726a5985e5ab2f57910bfd6bc4fa3c5631830
Thanh Nguyen <ehh_13olt3@hotmail.com>
2025-07-22 20:45:11 +1000

    amend: updated Player class to DRY it

--- src/index.js
+++ src/index.js
@@ -12,21 +12,21 @@ const startGame = document.querySelector("[start-game]");
 // });
 
 startGame.addEventListener("click", () => {
-  let playerOne = new Player();
+  let playerOne = new Player("p-one");
   playerOne.board.placeShip("b", 2, "down", 3);
   playerOne.board.placeShip("d", 10, "up", 4);
   playerOne.board.placeShip("j", 6, "left", 2);
   playerOne.board.placeShip("f", 3, "right", 5);
 
-  playerOne.renderBoardOne();
+  playerOne.renderBoard();
 
-  let playerTwo = new Player();
+  let playerTwo = new Player("p-two");
   playerTwo.board.placeShip("d", 2, "down", 5);
   playerTwo.board.placeShip("b", 10, "right", 4);
   playerTwo.board.placeShip("g", 8, "left", 3);
   playerTwo.board.placeShip("j", 2, "up", 2);
 
-  playerTwo.renderBoardTwo();
+  playerTwo.renderBoard();
   const instruct = document.querySelector("[instruction]");
   instruct.innerText = "Player one's turn to  attack";
 });
