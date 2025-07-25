0e83e1b2407aa1ca5ec1d8802e12ec5b6b8bf7c0
Thanh Nguyen <ehh_13olt3@hotmail.com>
2025-07-25 18:54:51 +1000

    feat: added popup button and proper turn functionality

--- src/game.js
+++ src/game.js
@@ -1,6 +1,9 @@
 import { Player } from "../script/battleships";
 
 export class Game {
+  constructor() {
+    this.gameState = undefined;
+  }
   startGame() {
     let playerOne = new Player("p-one");
     playerOne.board.placeShip("b", 2, "down", 3);
@@ -17,31 +20,128 @@ export class Game {
     playerTwo.board.placeShip("j", 2, "up", 2);
 
     playerTwo.renderBoard();
-    const instruct = document.querySelector("[instruction]");
-    instruct.innerText = "Player one's turn to  attack";
-    this.playerOneTurn(playerOne, playerTwo);
+
+    const start = document.querySelector("[start-instruction]");
+    start.style.display = "none";
+
+    //popup functionality
+    const popup = document.querySelector("[popup]");
+    const popupInstruction = document.querySelector("[instruction-next]");
+    const veil = document.querySelector("[veil]");
+    popupInstruction.innerText = `Game start. \nPlayer One's turn.`;
+    veil.style.zIndex = "3";
+    popup.style.zIndex = "4";
+
+    this.gameState = "p1turn";
+
+    this.buttonStart();
+
+    //button move logic should be in start game function
+    //turn function should JUST turn display on and off and change instruction
+
+    this.pOneButtonFunctionality(playerOne, playerTwo);
+    this.pTwoButtonFunctionality(playerOne, playerTwo);
   }
 
-  playerOneTurn(pOneObject, pTwoObject) {
-    let selector = `[p-one-attack] > div > div`;
-    const selectedCell = document.querySelectorAll(selector);
+  pOneButtonFunctionality(pOneObject, pTwoObject) {
+    const pOneselector = `[p-one-attack] > div > div`;
+    const selectedCell = document.querySelectorAll(pOneselector);
+
     selectedCell.forEach((square) => {
       square.addEventListener("click", (e) => {
+        const popup = document.querySelector("[popup]");
+        const popupInstruction = document.querySelector("[instruction-next]");
+        const veil = document.querySelector("[veil]");
         let attackCoord = e.target.attributes[0].name;
 
+        // checks if attacked cell of player two is occupied
         if (pTwoObject.board.occupiedCells.includes(attackCoord)) {
           square.style.backgroundColor = "green";
-          square.innerText = "hit!";
+          square.innerText = "O";
+          popupInstruction.innerText = `Hit!\nPlayer Two's turn.`;
         } else {
           square.style.backgroundColor = "red";
-          square.innerText = "miss!";
+          square.innerText = "X";
+          popupInstruction.innerText = `Miss!\nPlayer Two's turn.`;
         }
+        pOneObject.board.attackedCells.push(attackCoord);
+        veil.style.zIndex = "3";
+        popup.style.zIndex = "4";
 
         const otherBoard = document.querySelector(
           `[p-two-base] > div > [${e.target.attributes[0].name}]`
         );
         otherBoard.innerText = "x";
+        otherBoard.style.textAlign = "center";
+        this.gameState = "p2turn";
       });
     });
+  }
+
+  pTwoButtonFunctionality(pOneObject, pTwoObject) {
+    const pTwoselector = `[p-two-attack] > div > div`;
+    const selectedCell = document.querySelectorAll(pTwoselector);
+
+    selectedCell.forEach((square) => {
+      square.addEventListener("click", (e) => {
+        const popup = document.querySelector("[popup]");
+        const popupInstruction = document.querySelector("[instruction-next]");
+        const veil = document.querySelector("[veil]");
+        let attackCoord = e.target.attributes[0].name;
+
+        // checks if attacked cell of player two is occupied
+        if (pOneObject.board.occupiedCells.includes(attackCoord)) {
+          square.style.backgroundColor = "green";
+          square.innerText = "O";
+          popupInstruction.innerText = `Hit!\nPlayer One's turn.`;
+        } else {
+          square.style.backgroundColor = "red";
+          square.innerText = "X";
+          popupInstruction.innerText = `Miss!\nPlayer One's turn.`;
+        }
+        pTwoObject.board.attackedCells.push(attackCoord);
+        veil.style.zIndex = "3";
+        popup.style.zIndex = "4";
+
+        const otherBoard = document.querySelector(
+          `[p-one-base] > div > [${e.target.attributes[0].name}]`
+        );
+        otherBoard.innerText = "x";
+        otherBoard.style.textAlign = "center";
+        this.gameState = "p1turn";
+      });
+    });
+  }
+
+  showPOneBoard() {
+    const pOne = document.querySelector("[board-frame-one");
+    const pTwo = document.querySelector("[board-frame-two");
+    pOne.style.zIndex = "2";
+    pTwo.style.zIndex = "1";
+  }
+  showPTwoBoard() {
+    const pOne = document.querySelector("[board-frame-one");
+    const pTwo = document.querySelector("[board-frame-two");
+    pOne.style.zIndex = "1";
+    pTwo.style.zIndex = "2";
+  }
+
+  buttonStart() {
+    const button = document.querySelector("[popup-button]");
+    button.addEventListener("click", () => {
+      const popup = document.querySelector("[popup]");
+      const veil = document.querySelector("[veil]");
+      veil.style.zIndex = "0";
+      popup.style.zIndex = "0";
+      this.nextTurn(this.gameState);
+    });
+  }
+  nextTurn(gameState) {
+    if (gameState === "p1turn") {
+      this.showPOneBoard();
+    }
+    if (gameState === "p2turn") {
+      this.showPTwoBoard();
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import { Game } from "./game";
 import "./styles.css";
 const startGame = document.querySelector("[start-game]");
 
-// const base = document.querySelectorAll("[base-squares]>div>div");
-
-// base.forEach((square) => {
-//   square.addEventListener("click", () => {
-//     // console.log("5");
-//   });
-// });
-
 startGame.addEventListener("click", () => {
   let game = new Game();
   game.startGame();
